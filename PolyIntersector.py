7b4a03a995deaa0a8b2fba423956663583fc1dee
Tyler Hanks <teh.hanks@gmail.com>
2019-04-21 19:31:37 -0400

    add ConvPolyIntersection function and fix abunch of bugs

--- PolyIntersector.py
+++ PolyIntersector.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import pdb
 
 class Window:
     def __init__(self, in_dimension):
@@ -32,7 +33,7 @@ class Window:
             screen_poly = []
             for point in poly.points:
                 screen_poly.append(world_to_screen(point))
-            print(screen_poly)
+            #print(screen_poly)
             self.canvas.create_polygon(screen_poly, activefill = poly.color)
 
     def show(self):
@@ -43,171 +44,123 @@ class Polygon:
         self.points = in_points
         self.color = in_color
 
-def segmentIntersection(seg1, seg2):
-    a = seg1[0]
-    b = seg1[1]
-    c = seg2[0]
-    d = seg2[1]
+def ConvPolyIntersection(poly1, poly2):
+    """Computes the intersection of poly1 and poly2"""
+    #helper function definitions:
+    def vectorSub(v1, v2):
+        """Computes v1 - v2"""
+        return (v1[0] - v2[0], v1[1] - v2[1])
+    def vectorPlus(v1, v2):
+        """Computes v1 + v2"""
+        return (v1[0] + v2[0], v1[1] + v2[1])
+    def scalarMult(s, v):
+        """Computes scalar s multiplied by vector v"""
+        return (s*v[0], s*v[1])
+    def vectorCross(v1, v2):
+        """Computes the cross product of 2D vectors v1 and v2"""
+        return (v1[0] * v2[1] - v1[1] * v2[0])
+    def leftOf(a, b, c):
+        """Determines if point c is to the left of line ab"""
+        area = (b[0] - a[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (b[1] - a[1])
+        if area > 0:
+            return 1
+        elif area < 0:
+            return -1
+        else:
+            return 0
+    def segmentIntersection(p, p1, q, q1):
+        """Computes the intersection of pp1 and qq1. Returns () if no intersection. Does not handle degenerate cases"""
+        #pdb.set_trace()
+        r = vectorSub(p1, p) #pp1 = p + tr
+        s = vectorSub(q1, q) #qq1 = q + us
 
-    code = '?'
+        if vectorCross(r, s) == 0:
+            #print("Error: Degenerate segment intersection 0.")
+            return ()
+        
+        denom = float(vectorCross(r,s))
+        t = vectorCross(vectorSub(q,p), s) / denom #t = (q-p)*s/(r*s)
+        u = vectorCross(vectorSub(q,p), r) / denom #u = (q-p)*r/(r*s)
 
-    denom = a[0] * (d[1] - c[1]) + b[0] * (c[1] - d[1]) + d[0] * (b[1] - a[1]) + c[0] * (a[1] - b[1])
-    #denom = float(denom)
+        if (0.0 <= t <= 1.0) and (0.0 <= u <= 1.0):
+            return vectorPlus(p, scalarMult(t, r)) #p + tr
+        else:
+            #print("Error: Degenerate segment intersection 1.")
+            return ()
+    def isInHalfPlane(p, q1, q2): #note these may need to be flipped
+        """Checks if p is in the half plane of q1q2"""
+        r = leftOf(q1,q2,p)
+        if r == 1:
+            return True
+        else:
+            return False
 
-    if denom is 0:
-        print("error divide by 0")
-        return 0
-    
-    num = a[0] * (d[1] - c[1]) + c[0] * (a[1] - d[1]) + d[0] * (c[1] - a[1])
-    float(num)
-    if (num is 0.0) or (num is denom):
-        code = 'v'
-    s = num / denom
-    num = -	(a[0] * (c[1] - b[1]) + b[0] * (a[1] - c[1]) + c[0] * (b[1] - a[1]))
-    num = float(num)
-
-    if (num is 0.0) or (num is denom):
-        code = 'v'
-    t = num / denom
-
-    if 0.0 < s and s < 1.0 and 0.0 < t and t < 1.0:
-        code = '1'
-    elif 0.0 > s or s > 1.0 or 0.0 > t or t > 1.0:
-        code = '0'
-    
-    x = a[0] + s * (b[0] - a[0])
-    y = a[1] + t * (b[1] - a[1])
-
-    return (code, (x,y))
-
-def leftOf(a, b, c):
-    area = (b[0] - a[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (b[1] - a[1])
-
-    if area > 0:
-        return 1
-    elif area < 0:
-        return -1
-    else:
-        return 0
-
-def pointSub(point1, point2):
-    return (point2[0] - point1[0], point2[1], point1[1])
-
-def InOut(inflag, aHB, bHA):
-    if (aHB > 0):
-        return "Pin"
-    elif (bHA > 0):
-        return "Qin"
-    else:
-        return inflag
-
-def polyIntersection(P, Q):
+    #variable definitions
+    P = poly1.points
+    Q = poly2.points
     n = len(P)
     m = len(Q)
-    a = 0
-    b = 0
-    aa = 0
-    ba = 0
-    FirstPoint = True
-    inFlag = "Unknown"
+    p_index = 0
+    q_index = 0
+    inside = None
+    firstIntersectFlag = True #flag indicating whether this is the first intersection
+    firstIntersect = ()
     result = []
 
-    a1 = (a + n -1) % n
-    b1 = (b + m -1) % m
+    #main loop
+    for i in range(0, 2*(n+m)+1): #repeat no more that 2(|P|+|Q|) times
+        #pdb.set_trace()
+        p = P[p_index]
+        p_1 = P[(p_index + n - 1) % n] #p_1 is the vertex before p
 
-    A = pointSub(P[a], P[a1])
-    B = pointSub(Q[b], Q[b1])
+        q = Q[q_index]
+        q_1 = Q[(q_index + m - 1) % m] #q_1 is the vertex before q
 
-    cross = leftOf((0,0), A, B)
-    aHB = leftOf(Q[b1], Q[b], P[a])
-    bHA = leftOf(P[a1], P[a], Q[b])
-
-    res = segmentIntersection([P[a1], P[a]], [Q[b1], Q[b]])
-    code = res[0]
-    p = res[1]
-
-    if code is '1' or code is 'v':
-        if inFlag is "Unknown" and FirstPoint:
-            aa = ba = 0
-            FirstPoint = False
-            result.append((p[0], p[1]))
-        result.append(p)
-        inFlag = InOut(inFlag, aHB, bHA)
-
-    elif cross >= 0:
-        def Advance(a, aa, n, inside, v):
-            if inside:
-                result.append(v)
-            return (aa+1, (a+1) % n)
-        if bHA > 0:
-            r = Advance(a, aa, n, inFlag is "Pin", P[a])
-            aa = r[0]
-            a = r[1]
-        else:
-            r = Advance(b, ba, m, inFlag is "Qin", Q[b])
-            ba = r[0]
-            b = r[1]
-    else:
-        if aHB > 0:
-            r = Advance(b, ba, m, inFlag is "Qin", Q[b])
-            ba = r[0]
-            b = r[1]
-        else:
-            r = Advance(a, aa, n, inFlag is "Pin", P[a])
-            aa = r[0]
-            a = r[1]
-    
-    while((aa < n or ba < m) and (aa < 2 * n and ba < 2 * m)):
-        a1 = (a + n -1) % n
-        b1 = (b + m -1) % m
-
-        A = pointSub(P[a], P[a1])
-        B = pointSub(Q[b], Q[b1])
-
-        cross = leftOf((0,0), A, B)
-        aHB = leftOf(Q[b1], Q[b], P[a])
-        bHA = leftOf(P[a1], P[a], Q[b])
-
-        res = segmentIntersection([P[a1], P[a]], [Q[b1], Q[b]])
-        code = res[0]
-        p = res[1]
-
-        if code is '1' or code is 'v':
-            if inFlag is "Unknown" and FirstPoint:
-                aa = ba = 0
-                FirstPoint = False
-                result.append((p[0], p[1]))
-            result.append(p)
-            inFlag = InOut(inFlag, aHB, bHA)
-
-        elif cross >= 0:
-            def Advance(a, aa, n, inside, v):
-                if inside:
-                    result.append(v)
-                return (aa+1, (a+1) % n)
-            if bHA > 0:
-                r = Advance(a, aa, n, inFlag is "Pin", P[a])
-                aa = r[0]
-                a = r[1]
+        intersection = segmentIntersection(p_1, p, q_1, q) #note these may need to be flipped
+        if intersection != (): #if the two segments intersect
+            if firstIntersectFlag:
+                firstIntersect = intersection
+                firstIntersectFlag = False
+                result.append(intersection)
+                if isInHalfPlane(p, q_1, q):
+                    inside = "P"
+                else:
+                    inside = "Q"
+            elif intersection == firstIntersect: #we are done
+                return Polygon(result, 'purple')
             else:
-                r = Advance(b, ba, m, inFlag is "Qin", Q[b])
-                ba = r[0]
-                b = r[1]
-        else:
-            if aHB > 0:
-                r = Advance(b, ba, m, inFlag is "Qin", Q[b])
-                ba = r[0]
-                b = r[1]
+                result.append(intersection)
+                if isInHalfPlane(p, q_1, q):
+                    inside = "P"
+                else:
+                    inside = "Q"
+        
+        #advance one of the edges
+        if vectorCross(vectorSub(q, q_1), vectorSub(p, p_1)) >= 0:
+            if isInHalfPlane(p, q_1, q):
+                #advance(q)
+                if inside == "Q":
+                    result.append(q)
+                q_index = (q_index + 1) % m
             else:
-                r = Advance(a, aa, n, inFlag is "Pin", P[a])
-                aa = r[0]
-                a = r[1]
-    
-    if inFlag is "Unknown":
-        print("FUCK")  
+                #advance(p)
+                if inside == "P":
+                    result.append(p)
+                p_index = (p_index + 1) % n
+        else:
+            if isInHalfPlane(q, p_1, p):
+                #advance(p)
+                if inside == "P":
+                    result.append(p)
+                p_index = (p_index + 1) % n
+            else:
+                #advance(q)
+                if inside == "Q":
+                    result.append(q)
+                q_index = (q_index + 1) % m
 
-    return result
-   
+    #there was no intersection or a degenerate case
+    return Polygon([], "purple")
 
 def main():
     window = Window(600)
@@ -215,23 +168,22 @@ def main():
     #poly1 = Polygon([(0,16),(5,8),(13,0),(19,2),(24,10),(24,26),(19,29),(13,32),(7,32),(3,29),(0,16)], 'blue')
     #poly2 = Polygon([(28,16),(16,32),(0,22),(3,10),(16,-3),(28,16)], 'red')
 
-    poly1 = Polygon([(0,0), (5,0), (5,5)],'blue')
-    poly2 = Polygon([(1,-1),(3,-1),(3,3),(1,3)],'red')
+    #poly1 = Polygon([(0.0,0.0), (5.0,0.0), (5.0,5.0)],'blue')
+    #poly2 = Polygon([(1.0,-1.0),(4.0,-.5),(3.0,8.0),(2.0,6.0)],'red')
 
-    print("Init")
+    poly1 = Polygon([(0.0,0.0), (5.0,0.0), (5.0,5.0)],'blue')
+    poly2 = Polygon([(1.0,-1.0),(4.0,-.5),(4.25,2),(3.0,8.0),(2.0,6.0)],'red')
 
-    intersection = polyIntersection(poly1.points, poly2.points)
-    poly3 = Polygon(intersection, 'purple')
+    #poly1 = Polygon([(0,0),(3,0),(3,3),(0,3)], 'red')
+    #poly2 = Polygon([(2,1),(4,1),(4,4),(2,4)], 'blue')
+    poly3 = ConvPolyIntersection(poly1, poly2)
 
-    print("Didn't diverge")
+    if poly3.points != []:
+        window.drawPolys([poly1, poly2, poly3])
+        print(poly3.points)
+    else:
+        window.drawPolys([poly1, poly2])
 
-    window.drawPolys([poly3, poly2, poly1])
-    #test = Polygon
-    #poly1.draw(window)
-    #poly2.draw(window)
-
-    #print(signedArea((0,0),(1,1),(1,0)))
-    #window.drawPolys([poly2, poly1])
     window.show()
 
 if __name__=="__main__":
